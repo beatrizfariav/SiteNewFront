3b0e4b795a7d736969d10e0af6bed981ad0a6fcc
Beatriz de Faria Vargas <beatriz.fariav@gmail.com>
2021-03-02 14:47:05 -0300

    Finalização do menu + animações da home
    Terminei de ajustar o menu e adicionei as animações em GSAP na página home (assim como os floatings nas imagens).

--- js/app.js
+++ js/app.js
@@ -56,3 +56,113 @@ window.onload = function () {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+//NAVBAR - SHOW ON SCROLL UPDATE
+
+var lastScrollTop = 0;
+var desktopViewport = window.matchMedia("(min-width: 921px)");
+
+navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (desktopViewport.matches) {
+    if (scrollTop > lastScrollTop) {
+      navbar.style.top = "-6rem";
+    } else {
+      navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+  }
+});
+
+// GSAP ANIMATIONS
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".animate-hero", {
+  duration: 0.5,
+  opacity: 0,
+  y: -20,
+  stagger: 0.3,
+});
+
+gsap.from(".animate-services", {
+  scrollTrigger: {
+    trigger: ".animate-services",
+    start: "top bottom",
+    scrub: 0.1,
+  },
+  duration: 0.6,
+  opacity: 0,
+  x: -20,
+  stagger: 0.3,
+  delay: 0.5,
+});
+
+gsap.from(".animate-benefits", {
+  scrollTrigger: {
+    trigger: ".animate-benefits",
+    start: "top bottom",
+    scrub: 1,
+  },
+  duration: 0.6,
+  opacity: 0,
+  y: 20,
+  stagger: 0.3,
+  delay: 1.2,
+});
+
+gsap.from(".animate-agency", {
+  scrollTrigger: {
+    trigger: ".animate-agency",
+    start: "top bottom",
+    scrub: 1,
+  },
+  duration: 0.3,
+  opacity: 0,
+  x: -20,
+  stagger: 0.2,
+  delay: 1.3,
+});
+
+gsap.from(".animate-pricing", {
+  scrollTrigger: ".animate-benefits",
+  duration: 0.6,
+  opacity: 0,
+  y: 20,
+  stagger: 0.3,
+  delay: 1.2,
+});
+
+gsap.from(".animate-process", {
+  scrollTrigger: {
+    trigger: ".animate-process",
+    start: "top bottom",
+    scrub: 1,
+  },
+  duration: 0.3,
+  y: -20,
+  stagger: 0.2,
+});
+
+gsap.from(".animate-testimonial", {
+  scrollTrigger: {
+    trigger: ".animate-process",
+    start: "top bottom",
+    scrub: 1,
+  },
+  duration: 0.3,
+  y: -100,
+  stagger: 0.1,
+});
+
+gsap.from(".animate-contact", {
+  scrollTrigger: ".animate-contact",
+  duration: 0.6,
+  opacity: 0,
+  y: 20,
+  stagger: 0.3,
+  delay: 1.2,
+});
